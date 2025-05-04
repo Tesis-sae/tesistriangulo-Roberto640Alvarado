bc11fd9706916478002fd0f49138116e072636a7
Roberto Alvarado <62621556+Roberto640Alvarado@users.noreply.github.com>
2025-05-03 19:12:20 -0600

    Update ejercicio.cpp

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 
 void imprimirTriangulo(int altura) {
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
 
 int main() {
+    imprimirTriangulo(3);
+    imprimirTriangulo(2);
+    imprimirTriangulo(3);
 
     return 0;
 }
